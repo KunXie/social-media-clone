8b9ddf2497ac215e11cd9e11a1422e6cd32262e0
Kun Xie <alex.xiekun@gmail.com>
2021-03-20 14:57:06 -0500

    finish the backend part

---
+++ socialape-functions/dbschema.js
@@ -0,0 +1,56 @@
+let db = {
+  users: [
+    {
+      userId: "12312332esfsae",
+      email: "user@email.com",
+      handle: "user",
+      createdAt: "2019-03-15T10:59:52.789",
+      imageUrl: "image/;lkasledfkjhwqlkf",
+      bio: "Hello, my name is user, nice to meet you",
+      website: "https://user.com",
+      location: "London, UK",
+    },
+  ],
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2019-03-15T11:46:01.0182",
+      likeCount: 5,
+      commentCount: 3,
+    },
+  ],
+  notification: [
+    {
+      recipient: "user",
+      sender: "john",
+      read: "true | false",
+      screamId: "aslkf;jqwfkl;j",
+      type: "like | comment",
+      createdAt: "2019-03-15T10:59:52.7982",
+    },
+  ],
+};
+const useDetails = {
+  // Redux data
+  credentials: {
+    userId: "wqelrkjqwelr;kqjw;lr",
+    email: "",
+    handle: "user",
+    createdAt: "",
+    imageUrl: "",
+    bio: "",
+    website: "",
+    location: "Topeka, Kansas",
+  },
+  likes: [
+    {
+      userHandle: "user",
+      screamId: "wqlkjehrqwkljrh",
+    },
+    {
+      userHandle: "user",
+      screamId: "wqkl;erhqwekjh",
+    },
+  ],
+};
---
+++ socialape-functions/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyB0vBAgz8_fb3FsUXTrAnL0CWS6U_8jii8",
+  authDomain: "social-media-clone-60bff.firebaseapp.com",
+  projectId: "social-media-clone-60bff",
+  storageBucket: "social-media-clone-60bff.appspot.com",
+  messagingSenderId: "932310074532",
+  appId: "1:932310074532:web:bd700bea8aa3ac7f251231",
+  measurementId: "G-YVL2X6Q4X7",
+};
